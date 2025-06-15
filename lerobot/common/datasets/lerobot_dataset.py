83510d5e394b7387ac9537a0ca3d8128049b2e8e
Nora Abi Akar <noraabiakar@microsoft.com>
2025-06-15 15:05:19 +0200

    WIP

--- lerobot/common/datasets/lerobot_dataset.py
+++ lerobot/common/datasets/lerobot_dataset.py
@@ -473,6 +473,9 @@ class LeRobotDataset(torch.utils.data.Dataset):
         try:
             if force_cache_sync:
                 raise FileNotFoundError
+            for fpath in self.get_episodes_file_paths():
+                print(self.root / fpath)
+                print((self.root / fpath).is_file())
             assert all((self.root / fpath).is_file() for fpath in self.get_episodes_file_paths())
             self.hf_dataset = self.load_hf_dataset()
         except (AssertionError, FileNotFoundError, NotADirectoryError):
